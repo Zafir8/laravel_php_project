c45ddf5dea4c2e10e870b49070075ad439d5a8c5
Mohamed Zafir Sharaz <zafirsharaz@gmail.com>
2024-02-04 13:34:18 +0530

    page 2

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,11 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         \App\Models\User::factory(10)->create();
+        \App\Models\VehicleCategory::factory(10)->create();
+
+
+
+
 
         \App\Models\User::factory()->create([
             'name' => 'Admin',
@@ -20,6 +25,8 @@ class DatabaseSeeder extends Seeder
             'password' => bcrypt('admin'),
             'role' => 1
         ]);
+
+
 
 
     }
